28f32f029c953b2a3e1fa2407993a83720885497
Juan Esteban Acosta <Juan Esteban Acosta@DESKTOP-JFEGGGS>
2025-05-21 16:34:15 -0500

    mas

--- reactproject1/vite.config.js
+++ reactproject1/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [react()],
+    build: {
+        outDir: 'dist'
+    },
     server: {
-        port: 55480,
+        port: 5173,
+        open: true
     }
-})
+});
